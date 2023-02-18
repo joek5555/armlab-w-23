075480036c9fb67dd4ac8dc5423c157d48ff4983
Joseph Kennedy <joekennedy5555@gmail.com>
2023-02-17 20:22:31 -0500

    Add joint plots to plot_end_effector_FK.py

--- plot_end_effector_FK.py
+++ plot_end_effector_FK.py
@@ -38,6 +38,13 @@ y = np.zeros(len(list_of_lines))
 z = np.zeros(len(list_of_lines))
 i = 0
 
+# create arrays to store joint data
+joint1 = np.zeros(len(list_of_lines))
+joint2 = np.zeros(len(list_of_lines))
+joint3 = np.zeros(len(list_of_lines))
+joint4 = np.zeros(len(list_of_lines))
+joint5 = np.zeros(len(list_of_lines))
+
 # for each line in the text file, get the joint angles, pass through forward kinematics, and store end effector position
 for joint_position_str in list_of_lines:
     
@@ -47,10 +54,22 @@ for joint_position_str in list_of_lines:
 
     Tq = kinematics.FK_pox(joint_angles, M_matrix, S_list)
     pos = kinematics.get_pose_from_T(Tq)
+
+    # store end effector position
     x[i] = pos[0]
     y[i] = pos[1]
     z[i] = pos[2]
+
+    # store joint angles
+    joint1[i] = joint_angles[0]
+    joint2[i] = joint_angles[1]
+    joint3[i] = joint_angles[2]
+    joint4[i] = joint_angles[3]
+    joint5[i] = joint_angles[4]
+
     i += 1
+
+    
 
 
 # plot the end effector position
@@ -62,4 +81,35 @@ ax.set_ylabel('y')
 ax.set_zlabel('z')
 ax.scatter3D(x, y, z, )
 
+t = np.arange(0, 500, 1)
+
+plt.show()
+
+fig, axs = plt.subplots(3,2)
+axs[0,0].plot(t,joint1)
+#axs[0,0].set_xlabel('timestep')
+axs[0,0].set_ylabel('radians')
+axs[0,0].set_title('Joint 1')
+
+axs[0,1].plot(t, joint2)
+#axs[0,1].set_xlabel('timestep')
+axs[0,1].set_ylabel('radians')
+axs[0,1].set_title('Joint 2')
+
+axs[1,0].plot(t, joint3)
+#axs[1,0].set_xlabel('timestep')
+axs[1,0].set_ylabel('radians')
+axs[1,0].set_title('Joint 3')
+
+axs[1,1].plot(t, joint4)
+#axs[1,1].set_xlabel('timestep')
+axs[1,1].set_ylabel('radians')
+axs[1,1].set_title('Joint 4')
+
+axs[2,0].plot(t, joint5)
+#axs[2,0].set_xlabel('timestep')
+axs[2,0].set_ylabel('radians')
+axs[2,0].set_title('Joint 5')
+
+plt.delaxes(axs[2,1])
 plt.show()
